a122438185b13ef8c5197da30be50825d0c00d89
EvgeniZhibrik <evgeni.zhibrik@gmail.com>
2016-07-10 16:01:08 +0300

    initial state

---
+++ server/server.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var express = require('express');
+var path = require('path');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+app.use(express.static('../client'));
+
+app.get('/*', function (req, res) {
+ res.sendFile(path.resolve(__dirname, '../client/index.html'));
+});
+
+app.listen(3000, function () {
+ console.log('Example app listening on port 3000!');
+});
